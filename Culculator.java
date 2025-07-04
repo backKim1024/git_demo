e5485996b244e1e1f53333624e4f8f6baac85054
backKim1024 <dydgml7313@naver.com>
2025-07-04 09:43:40 +0900

    feat: modify calculater

--- Culculator.java
+++ Culculator.java
@@ -1,5 +1,7 @@
 public class Calculator {
-	public int add(int a, int b) {
+	public int add(String a, String b) {
+		int a = Integer.parseInt(a);
+		int b = Integer.parseInt(b);
 		return a + b;
 	}
 }
