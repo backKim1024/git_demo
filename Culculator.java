c25aac6d1aa288fd7dbc744cf746a3f3dd4cff77
backKim1024 <dydgml7313@naver.com>
2025-07-04 10:04:56 +0900

    feat: modify calculater2

--- Culculator.java
+++ Culculator.java
@@ -2,6 +2,8 @@ public class Culculator  {
 	public int add(String a, String b) {
 		int a = Integer.parseInt(a);
 		int b = Integer.parseInt(b);
+		long A = (long) a;
+		long B = (long) b;
 		return a + b;
 	}
 }
